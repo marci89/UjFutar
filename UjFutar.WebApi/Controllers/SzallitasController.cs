88a30c2fca7fc2e9088c7a80541a7b5ea253a223
noname <noname@nomail.com>
2022-10-20 08:46:24 +0200

    WebApi létrehozása

---
+++ UjFutar.WebApi/Controllers/SzallitasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using UjFutar.Models;
+
+namespace UjFutar.WebApi.Controllers
+{
+	/// <summary>
+	/// Szállítás menedzseléséhez készült Controller
+	/// </summary>
+	[Description("Szállítás menedzselése")]
+	[Route("[controller]")]
+	[ApiController]
+	public class SzallitasController : Controller
+	{
+
+        /// <summary>
+        /// Szállitasi adatok rögzítése
+        /// </summary>
+        [Description("Szállitasi adatok rögzítése")]
+        [HttpPost]
+        public IActionResult SzallitasiAdatokatRogzit([FromBody] SzallitasiAdatokatRogzitKeresModel request)
+        {
+			return Ok();
+        }
+
+    }
+}
